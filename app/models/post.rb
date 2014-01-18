23e85bb47651c5c92ba43928eb813c5ce57cd7c6
SanaullaParvez <sanaulla.ict@gmail.com>
2014-01-18 11:55:48 +0600

    Photo Destroy Permition

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
 
   attr_accessible :description, :image, :price, :title, :order, :category_ids, :images_attributes , :user_id
   has_many :category_posts
-  has_many :images
+  has_many :images, :dependent => :destroy
   has_many :categories, through: :category_posts
   belongs_to :user
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, :allow_destroy => true
 end
